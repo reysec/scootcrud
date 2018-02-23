bc7cac2b92195caac3de0d92d7843b138aa0365d
Angga Pramudya <pramudya.angga58@gmail.com>
2018-02-23 19:10:48 +0700

    Adding CREATE operation

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,3 +1,8 @@
 module.exports = function(app,db) {
-    
+    app.post('/notes', (req, res) => {
+        //Create your note in here
+        console.log(req.body);
+        res.send('Hello')
+    })
 }
+
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser')
 const app = express();
 const port = 8000
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 require('./app/routes')(app, {})
 app.listen(port, () => {
     console.log('We are on port '+port);
