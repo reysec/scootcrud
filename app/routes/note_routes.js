572ea049af57eaf4216298d2d46ca04640e31bf6
Angga Pramudya <pramudya.angga58@gmail.com>
2018-02-24 22:26:18 +0700

    Adding UPDATE Operation & Route

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,6 +1,20 @@
 var ObjectID = require('mongodb').ObjectID
 
 module.exports = function(app,db) {
+    // CREATE Operation
+    app.post('/notes', (req, res) => {
+        const note = { text: req.body.body, title: req.body.title }
+
+        db.collection('notes').insert(note, (err, result) => {
+            if (err) {
+                res.send({ 'error': 'An error has occurred' })
+            } else {
+                res.send(result.ops[0])
+            }
+        })
+    })
+    
+    // READ Operation
     app.get('/notes/:id', (req, res) => {
         const id = req.params.id
         const details = { '_id': new ObjectID(id) }
@@ -14,18 +28,22 @@ module.exports = function(app,db) {
         })
     })
 
-    app.post('/notes', (req, res) => {
-        const note = { text: req.body.body, title: req.body.title }
+    // UPDATE Operation
+    app.put('/notes/:id', (req, res) => {
+        const id = req.params.id
+        const details = {'_id': new ObjectID(id)}
+        const note = {text: req.body.body, title: req.body.title}
 
-        db.collection('notes').insert(note, (err, result) => {
+        db.collection('notes').update(details, note, (err, result) => {
             if (err) {
-                res.send({ 'error': 'An error has occurred' })
+                res.send({'error': 'An error has occurred'})
             } else {
-                res.send(result.ops[0])
+                res.send(note)
             }
         })
     })
 
+    // DELETE Operation
     app.delete('/notes/:id', (req, res) => {
         const id = req.params.id;
         const details = {'_id': new ObjectID(id)}
