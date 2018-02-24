d8c1a0bc917bec85c943b6c11b71f02480947716
Angga Pramudya <pramudya.angga58@gmail.com>
2018-02-24 21:55:22 +0700

    Adding READ Operation & Route

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,5 +1,18 @@
+var ObjectID = require('mongodb').ObjectID
+
 module.exports = function(app,db) {
-    const collection =
+    app.get('/notes/:id', (req, res) => {
+        const id = req.params.id
+        const details = { '_id': new ObjectID(id) }
+        
+        db.collection('notes').findOne(details, (err, item) => {
+            if (err) {
+                res.send({'error': 'An error has occurred'})
+            } else {
+                res.send(item)
+            }
+        })
+    })
 
     app.post('/notes', (req, res) => {
         const note = { text: req.body.body, title: req.body.title }
